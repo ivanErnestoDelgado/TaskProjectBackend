bfd8e7f13334cf2432cabfb06f79a563b3073942
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2025-08-09 21:43:39 -0700

    Validation schema file added in task module, it contains the zod schemas will be applied for body and query params validation

---
+++ src/features/task/task.validator.js
@@ -0,0 +1,35 @@
+const { z } = require('zod');
+
+
+//objetos zod con validaciones predefinidas para no repetir codigo xdxdxd
+const titleStandarZodValidator=z.string().min(1,"The title is required").max(100,"Max number of allowed title characters is 100");
+
+const descriptionStandarZodValidator=z.string().max(500,"Max number of allowed description characters is 500");
+
+const numberFromStringZodValidator=z.string().regex(/^\d+$/,"THe input must be a number");
+
+
+//Esquemas para validacion del body y query params en las request
+const createTaskSchema= z.object({
+    title: titleStandarZodValidator,
+    description: descriptionStandarZodValidator.optional(),
+});
+
+const updateTaskSchema=z.object({
+    title: titleStandarZodValidator.optional(),
+    description: descriptionStandarZodValidator.optional(),
+    completed: z.boolean().optional(),
+});
+
+const getTaskQuerySchema=z.object({
+    completed: z.enum(['true','false'],"Completed query param must be 'true' or 'false'").optional(),
+    search: z.string().max(100,"Max number of allowed search characters is 100").optional(),
+    page: numberFromStringZodValidator.optional(),
+    limit: numberFromStringZodValidator.optional(),
+});
+
+module.exports={
+    createTaskSchema,
+    updateTaskSchema,
+    getTaskQuerySchema
+};
